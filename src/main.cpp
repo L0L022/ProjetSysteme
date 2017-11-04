0b0cc247a3abda32a83fdd8f2a0b038132b19543
L0L022 <L0L022@openmailbox.org>
2017-11-04 09:57:07 +0100

    main fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "../include/Maths.hpp"
 #include "../include/Object.hpp"
+#include "../include/SequentialNormalCalculation.hpp"
 #include "../include/Vector.hpp"
 #include <algorithm>
 #include <fstream>
@@ -12,38 +13,19 @@ using namespace std;
 int main(int argc, char *argv[]) {
   ifstream fichier(argv[1], std::ios::binary);
   stringstream ss;
-  //ss << fichier.rdbuf();
+  ss << fichier.rdbuf();
 
   cout.precision(std::numeric_limits<double>::digits10 + 1);
   cin.precision(std::numeric_limits<double>::digits10 + 1);
 
-  //Object obj = Object::readOFF(ss);
-  Object obj = Object::readOFF(fichier);
+  Object obj = Object::readOFF(ss);
 
-  cout << obj.faces().size() << ' ' << obj.points().size() << endl;
+  cout << "Nombre de faces : " << obj.faces().size()
+       << " Nombre de sommets : " << obj.vertices().size() << endl;
 
-  vector<Vector> normales(obj.faces().size());
-  for (int i = 0; i < normales.size(); ++i)
-    normales[i] = normale(obj, i);
+  SequentialNormalCalculation calc(obj);
+  calc.calculate();
 
-  vector<Point> sumPoint(obj.points().size());
-  vector<int> nbPoint(obj.points().size(), 0);
-  for (int i = 0; i < normales.size(); ++i) {
-    const Face &f = obj.faces()[i];
-    sumPoint[f.s0] += normales[i];
-    ++nbPoint[f.s0];
-
-    sumPoint[f.s1] += normales[i];
-    ++nbPoint[f.s1];
-
-    sumPoint[f.s2] += normales[i];
-    ++nbPoint[f.s2];
-  }
-
-  vector<Point> normPoint(obj.points().size());
-  for (int i = 0; i < normPoint.size(); ++i)
-    normPoint[i] = sumPoint[i] / nbPoint[i];
-
-    ofstream f("truc.obj", std::ios::binary | std::ios::out);
-    obj.writeOBJ(f);
+  ofstream f("truc.obj", std::ios::binary | std::ios::out);
+  obj.writeOBJ(f, calc.vertexNormal());
 }
