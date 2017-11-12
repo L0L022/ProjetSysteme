c6352660a3bd206c74cb51b3b2eb3ff949792c3f
L0L022 <L0L022@openmailbox.org>
2017-11-12 18:03:13 +0100

    cli ajout OpenMP

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,23 +1,43 @@
 #include <algorithm>
 #include <fstream>
 #include <limits>
+#include <memory>
 #include <sstream>
+#include <string>
 #include <vector>
 
 #include <lib/Maths.hpp>
+#include <lib/NormalCalculation.hpp>
 #include <lib/Object.hpp>
-#include <lib/SequentialNormalCalculation.hpp>
 #include <lib/Vector.hpp>
 
 using namespace std;
 using namespace lib;
 
+void
+printHelp()
+{
+  cout << "Usage : cli <OFF file> [sequential(default)|pthread|openmp]" << endl;
+}
+
 int
 main(int argc, char* argv[])
 {
-  ifstream fichier(argv[1], std::ios::binary);
+  if (argc != 2 and argc != 3) {
+    printHelp();
+    return -1;
+  }
+
+  ifstream file(argv[1], std::ios::binary);
+
+  if (!file.is_open()) {
+    printHelp();
+    cout << "Can't open OFF file.\n";
+    return -1;
+  }
+
   stringstream ss;
-  ss << fichier.rdbuf();
+  ss << file.rdbuf();
 
   cout.precision(std::numeric_limits<double>::digits10 + 1);
   cin.precision(std::numeric_limits<double>::digits10 + 1);
@@ -27,9 +47,25 @@ main(int argc, char* argv[])
   cout << "Nombre de faces : " << obj.faces().size()
        << " Nombre de sommets : " << obj.vertices().size() << endl;
 
-  SequentialNormalCalculation calc(obj);
-  calc.calculate();
+  auto method = NormalCalculation::Method::Sequential;
 
-  ofstream f("truc.obj", std::ios::binary | std::ios::out);
-  obj.writeOBJ(f, calc.vertexNormal());
+  if (argc == 3) {
+    string strMethod(argv[2]);
+    if (strMethod == "sequential")
+      method = NormalCalculation::Method::Sequential;
+    if (strMethod == "pthread") method = NormalCalculation::Method::pThread;
+    if (strMethod == "openmp") method = NormalCalculation::Method::OpenMP;
+  }
+
+  unique_ptr<NormalCalculation> calc(NormalCalculation::factory(method, obj));
+  if (calc) {
+    calc->calculate();
+
+    ofstream f("truc.obj", std::ios::binary | std::ios::out);
+    obj.writeOBJ(f, calc->vertexNormal());
+  } else {
+    printHelp();
+    cout << "Calculation not implemented.\n";
+    return -1;
+  }
 }
